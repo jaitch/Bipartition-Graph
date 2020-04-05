c65b2a512354ae7a075bd479ea0e75e2b6368c04
Janice Huang <janicehuang@gmail.com>
2020-04-05 11:01:34 -0700

    start two possible implementations. need searched if use each_with_index?

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,41 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  graph_queue = []
+  dislikes.each do |dog|
+    (graph_queue << dog).flatten!
+  end
+  searched = []
+  while !graph_queue.empty?
+
+  print graph_queue
 end
+
+def conflicting?()
+end
+
+#   first_group = []
+#   second_group = []
+
+#   dislikes.each_with_index {|enemies, dog|
+#     if !first_group.include?(dog) && !second_group.include?(enemies)
+#       first_group << dog
+#       (second_group << enemies).flatten!
+#     elsif !first_group.include?(enemies) && !second_group.include?(dog)
+#       first_group << enemies
+#       (second_group << dog).flatten!
+#     else
+#       return false
+#     end
+#     puts "group1: #{first_group}; group2: #{second_group}"
+#   }
+#   return true
+# end
+
+possible_bipartition(dislikes = [ [3, 6],
+  [2, 5],
+  [1, 3],
+  [0, 2],
+  [5],
+  [1, 4],
+  [0]
+  ])
